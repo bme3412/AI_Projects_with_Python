2d40a4689cf2abfcec1d8273571ac676a57324ff
bme3412 <54747855+bme3412@users.noreply.github.com>
2023-12-23 20:11:41 -0500

    Add files via upload

---
+++ LangchainProjects/banker.py
@@ -0,0 +1,86 @@
+# Import os to set API key
+import os
+# Import OpenAI as main LLM service
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+# Bring in streamlit for UI/app interface
+import streamlit as st
+import getpass
+
+
+# Import PDF document loaders...there's other ones as well!
+from langchain.document_loaders import PyPDFLoader
+# Import chroma as the vector store 
+from langchain.vectorstores import Chroma
+
+# Import vector store stuff
+from langchain.agents.agent_toolkits import (
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo
+)
+
+# Set APIkey for OpenAI Service
+# Can sub this out for other LLM providers
+os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key:')
+
+# Create instance of OpenAI LLM
+llm = OpenAI(temperature=0.1, verbose=True)
+embeddings = OpenAIEmbeddings()
+
+# Create and load PDF Loader
+loader = PyPDFLoader('ABNB_10K_2023.pdf')
+# Split pages from pdf 
+pages = loader.load_and_split()
+# Load documents into vector database aka ChromaDB
+
+TOKEN_LIMIT = 4096
+
+def estimate_tokens(text):
+    return len(text.split())
+
+processed_pages = []
+
+for page in pages:
+    if estimate_tokens(page) <= TOKEN_LIMIT: 
+        # Process the page as needed, e.g., with a language model
+        processed_content = process_page_with_language_model(page)
+        processed_pages.append(processed_content)
+    else:
+        # Optionally handle pages that exceed the token limit
+        # For example, splitting the page further or summarizing it
+        pass
+
+
+# Create vectorstore info object - metadata repo?
+vectorstore_info = VectorStoreInfo(
+    name="annual_report",
+    description="a banking annual report as a pdf",
+    vectorstore=store
+)
+# Convert the document store into a langchain toolkit
+toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+
+# Add the toolkit to an end-to-end LC
+agent_executor = create_vectorstore_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True
+)
+st.title('🦜🔗 GPT Investment Banker')
+# Create a text input box for the user
+prompt = st.text_input('Input your prompt here')
+
+# If the user hits enter
+if prompt:
+    # Then pass the prompt to the LLM
+    response = agent_executor.run(prompt)
+    # ...and write it out to the screen
+    st.write(response)
+
+    # With a streamlit expander  
+    with st.expander('Document Similarity Search'):
+        # Find the relevant pages
+        search = store.similarity_search_with_score(prompt) 
+        # Write out the first 
+        st.write(search[0][0].page_content) 
